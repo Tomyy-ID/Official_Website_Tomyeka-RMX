4490ec7803998cf6ed648a3da41af5e76583a759
Tomex.ID <tomyekachannel@gmail.com>
2026-05-22 21:25:48 +0700

    Add files via upload

--- FLM/data.js
+++ FLM/data.js
@@ -1,25 +1,36 @@
 const projects = [
 
 {
-  no: 1,
-  title: "DJ TRAP ARABIC INNI UHIBBUKA",
+  no: 3,
+  title: "DJ TRAP ARABIC INNI UHIBBUKA_TOMYEKA-RMX.zip",
   info: "Vocal + FLM",
   type: "ZIP",
-  size: "11 MB",
-  date: "Jumat, 22 Mei 2026",
+  size: "11,8 MB",
+  date: "Sabtu, 16 Mei 2026",
   videoid: "IfAjHb5YUo4",
   link: "https://sub4unlock.co/3FrarU"
 },
 
 {
   no: 2,
-  title: "DJ SLOW BASS TERBARU",
-  info: "FLM Only",
+  title: "DJ TRAP YA DANA DANA_TOMY-EKA-RMX.zip",
+  info: "Vocal + FM",
   type: "ZIP",
-  size: "9 MB",
-  date: "Sabtu, 23 Mei 2026",
-  videoid: "xxxxxxxx",
-  link: "https://example.com"
-}
+  size: "8,6 MB",
+  date: "Rabu, 6 Mei 2026",
+  videoid: "RBXPIe9ybaU?si=2rHeWfr5oYe1VGU-",
+  link: "https://sub4unlock.co/i2mO"
+},
+
+{
+  no: 1,
+  title: "DJ TRAP SHOLATUMMINALLAH_TOMYEKA-RMX.zip",
+  info: "Vocal + FM",
+  type: "ZIP",
+  size: "6,8 MB",
+  date: "Minggu, 26 April 2026",
+  videoid: "kKOXsIuLExg?si=8EMtmD6HBR0DlKw1",
+  link: "https://sub4unlock.co/LlUX9VdT"
+},
 
 ];
