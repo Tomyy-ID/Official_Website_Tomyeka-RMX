b44745263261b6daee63b6497a6e9affb4010f2b
Tomex.ID <tomyekachannel@gmail.com>
2026-05-29 06:39:45 +0700

    Add files via upload

--- FLM/data.js
+++ FLM/data.js
@@ -1,5 +1,30 @@
 const projects = [
 
+
+{
+  id: "1",
+  no: 1,
+  title: "DJ TRAP YA DANA DANA_TOMY-EKA-RMX.zip",
+  info: "Vocal + FLM",
+  type: "ZIP",
+  size: "8,6 MB",
+  date: "Rabu, 6 Mei 2026",
+  videoid: "RBXPIe9ybaU",
+  link: "https://sub4unlock.co/i2mO"
+},
+
+{
+id: "2",
+  no: 2,
+  title: "DJ TRAP SHOLATUMMINALLAH_TOMYEKA-RMX.zip",
+  info: "Vocal + FLM",
+  type: "ZIP",
+  size: "6,8 MB",
+  date: "Minggu, 26 April 2026",
+  videoid: "kKOXsIuLExg",
+  link: "https://sub4unlock.co/LlUX9VdT"
+},
+
 {
   id: "3",
   no: 3,
@@ -13,27 +38,15 @@ const projects = [
 },
 
 {
-  id: "2",
-  no: 2,
-  title: "DJ TRAP YA DANA DANA_TOMY-EKA-RMX.zip",
-  info: "Vocal + FLM",
+  id: "4",
+  no: 4,
+  title: "DJ TRAP SHOLAWAT PASRAH_TOMYEKA-RMX.zip",
+  info: "Vocal + FLM + Sample",
   type: "ZIP",
-  size: "8,6 MB",
-  date: "Rabu, 6 Mei 2026",
-  videoid: "RBXPIe9ybaU",
-  link: "https://sub4unlock.co/i2mO"
-},
-
-{
-id: "1",
-  no: 1,
-  title: "DJ TRAP SHOLATUMMINALLAH_TOMYEKA-RMX.zip",
-  info: "Vocal + FLM",
-  type: "ZIP",
-  size: "6,8 MB",
-  date: "Minggu, 26 April 2026",
-  videoid: "kKOXsIuLExg",
-  link: "https://sub4unlock.co/LlUX9VdT"
+  size: "21,03 MB",
+  date: "Jum'at, 29 Mei 2026",
+  videoid: "9bL_EHvdUCA",
+  link: "https://sub4unlock.co/gU2N8"
 },
 
 ];
